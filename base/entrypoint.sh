652bed82948da80bd1e96351ac38ec124c32fd21
Jean Claveau <1556489+jclaveau@users.noreply.github.com>
2025-10-17 12:07:18 +0200

    feat: add pnpm and simplify and update ubuntu

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 
+# TODO add the user given with the option --user to docker group
+# https://stackoverflow.com/questions/51342810/how-to-fix-dial-unix-var-run-docker-sock-connect-permission-denied-when-gro
+echo "User $(id -u):$(id -g)"
+
+
+# /!\ Very dangerous, do not use this outside dev env
+# https://stackoverflow.com/a/54568062/2714285
+sudo chmod 777 /var/run/docker.sock
+
+usermod -aG docker "$(id -u)"
+
+
 # Start Docker daemon in background
 dockerd-entrypoint.sh &
 
